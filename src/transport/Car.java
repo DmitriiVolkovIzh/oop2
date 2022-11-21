e223108ff21cbd576f312d045eec90288a86d4e9
DmitriiVolkovIzh <booyakaa@mail.ru>
2022-11-22 00:51:00 +0400

    наследование

--- src/transport/Car.java
+++ src/transport/Car.java
@@ -2,75 +2,47 @@ package transport;
 
 import java.time.LocalDate;
 
-public class Car {
-    private final String brand;
-    private final String model;
+public class Car extends Transport {
     private double engineVolume;
-    private String color;
-    private final int productionYear;
-    private final String productionCountry;
     private String transmission;
     private final String bodyType; // тип кузова
     private String registrationNumber;
     private final int numberOfSeats;
     private String tire;
 
-    private String noInfo = "default";
-
     public void infoCar() {
-        System.out.println(brand + " " + model + ", " + productionYear + " год выпуска, сборка " + productionCountry + ", " + color + " цвета, объем двигателя — "
+        System.out.println(getBrand() + " " + getModel()+ ", " + getProductionYear() + " год выпуска, сборка " + getProductionCountry() + ", " + getColor()+ " цвета, объем двигателя — "
                 + engineVolume + " л.с. Коробка передач - "+transmission+". Тип кузова - "
                 + bodyType + ". Регистрационный номер - "+ registrationNumber+ ". Кол-во мест: "+numberOfSeats+". Сезоность резины - "+tire);
     }
 
-    public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry, String transmission, String bodyType, String registrationNumber, int numberOfSeats, String tire) {
-        if (brand == null || brand.isEmpty()) this.brand = noInfo;
-        else this.brand = brand;
-        if (model == null || model.isEmpty()) this.model = noInfo;
-        else this.model = model;
+    public Car(String brand, String model, double engineVolume, String color,
+               int productionYear, String productionCountry, String transmission,
+               String bodyType, String registrationNumber, int numberOfSeats, String tire) {
+        super(brand,model,productionYear,productionCountry,color);
+
         if (engineVolume <= 0) this.engineVolume = 1.5;
         else this.engineVolume = engineVolume;
-        if (color == null || color.isEmpty()) this.color = "белого";
-        else this.color = color;
-        if (productionYear <= 0) this.productionYear = 2000;
-        else this.productionYear = productionYear;
-        if (productionCountry == null || productionCountry.isEmpty()) this.productionCountry = noInfo;
-        else this.productionCountry = productionCountry;
-        if (transmission == null || transmission.isEmpty()) this.transmission = noInfo;
+
+        if (transmission == null || transmission.isEmpty()) this.transmission = "информация не указана";
         else this.transmission = transmission;
-        if (bodyType == null || bodyType.isEmpty()) this.bodyType = noInfo;
+
+        if (bodyType == null || bodyType.isEmpty()) this.bodyType = "информация не указана";
         else this.bodyType = bodyType;
-        if (registrationNumber.length()<8) this.registrationNumber = noInfo;
+
+        if (registrationNumber.length()<8) this.registrationNumber = "информация не указана";
         else this.registrationNumber = registrationNumber;
+
         if (numberOfSeats <= 3 || numberOfSeats > 5 ) this.numberOfSeats = 5;
         else this.numberOfSeats = numberOfSeats;
+
         if (LocalDate.now().getDayOfMonth()<11 || LocalDate.now().getDayOfMonth()<3 ) this.tire = "летняя";
         else this.tire = "зимняя";
-    }
-
-    public String getBrand() {return brand;
-    }
-
-    public String getModel() {
-        return model;
     }
 
     public double getEngineVolume() {
         return engineVolume;
     }
-
-    public String getColor() {
-        return color;
-    }
-
-    public int getProductionYear() {
-        return productionYear;
-    }
-
-    public String getProductionCountry() {
-        return productionCountry;
-    }
-
     public String getTransmission() {
         return transmission;
     }
@@ -95,23 +67,16 @@ public class Car {
     public void setEngineVolume(double engineVolume) {
         this.engineVolume = engineVolume;
     }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
     public void setTransmission(String transmission) {
         this.transmission = transmission;
     }
-
     public void setRegistrationNumber(String registrationNumber) {
-        if (registrationNumber.length()<8) this.registrationNumber = noInfo;
+        if (registrationNumber.length()<8) this.registrationNumber = "информация не указана";
         else this.registrationNumber = registrationNumber;
         }
-
     public void setTire(String tire) {
         if (LocalDate.now().getDayOfMonth()<11 || LocalDate.now().getDayOfMonth()<3 ) this.tire = "летняя";
         else this.tire = "зимняя";
-
     }
+
 }
--- src/transport/Main.java
+++ src/transport/Main.java
@@ -19,6 +19,23 @@ public class Main {
         Car hyundai = new Car("Hyundai","Avante",1.6,"оранжевого",2016,"Южная Корея","РКПП","Седан","О24ВТ0716",1,"зимняя");
         hyundai.infoCar();
 
+        Train lastochka = new Train("Ласточка","B-901",2011,"",301,-3500,5,"Белорусского вокзала","Минск-Пассажирский",11);
+        lastochka.infoTrain();
+
+        Train leningrad = new Train("Ленинград","D-125",2019,"",270,1700,5,"Ленинградского вокзала","Ленинград-Пассажирский",8);
+        leningrad.infoTrain();
+
+        Bus ikarus = new Bus("Икарус","256",2001,"Венгрия","");
+        ikarus.infoBus();
+
+        Bus paz = new Bus("ПАЗ","4230",2003,"","Оранжевый");
+        paz.infoBus();
+
+        Bus bogdan = new Bus("Богдан","А092 ",2012,"Украина","желтого");
+        bogdan.infoBus();
+
+
+
 
 
 
